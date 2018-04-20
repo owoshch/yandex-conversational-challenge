a9733b24486bef23dc1899c2d5f55117f8e291f4
Fedor Kitashov <fedor.kitashov@phystech.edu>
2018-04-20 15:44:18 -0700

    separate evaluate

--- separate_evaluate.py
+++ separate_evaluate.py
@@ -7,9 +7,11 @@ import os
 import tensorflow as tf
 from ast import literal_eval
 import tqdm
-from separate_NER_model import NERModel
+#from separate_NER_model import NERModel
+from separate_variate_batch_model import NERModel
 from model.separate_config import Config
-from model.data_utils import load_pairwise_testset, sort_xgb_predictions, save_submission 
+from model.data_utils import load_pairwise_dataset, load_pairwise_testset, sort_xgb_predictions, save_submission 
+
 
 
 config = Config()
@@ -20,8 +22,14 @@ print (config.dir_model)
 
 model.restore_session(config.dir_model)
 
+train = load_pairwise_dataset(model.config.path_to_preprocessed_train)
+train_preds = model.predict_proba(train)
+np.save("../data/regressor_nn_train_preds.npy", train_preds) 
+
+
 
 sub = load_pairwise_testset(model.config.path_to_preprocessed_test)
-sub_preds = model.predict_proba(sub)
+sub_preds = model.predict_proba(sub, pd.read_csv(model.config.path_to_preprocessed_test))
+np.save("../data/regressor_nn_sub_preds.npy", sub_preds)
 sub_df = pd.read_csv(model.config.path_to_preprocessed_test)
-save_submission("../data/nn_separate_regression_train_1_epoch_test.txt", sub_df, sort_xgb_predictions(sub_df, sub_preds))
+save_submission("../data/nn_separate_regression_train_10_epoch_val.txt", sub_df, sort_xgb_predictions(sub_df, sub_preds))
